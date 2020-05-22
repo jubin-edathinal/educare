a2f3c43a6f4e585817e5436e2c6a889835904c16
Jubin.Jose <Jubin.Jose@CALMIL008.TSACORP.NET>
2020-05-22 15:39:56 +0530

    Initial commit.Created spring boot project & Configure application properties.

---
+++ educare/src/main/java/in/spotlight/educare/EducareApplication.java
@@ -0,0 +1,13 @@
+package in.spotlight.educare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducareApplication.class, args);
+	}
+
+}
